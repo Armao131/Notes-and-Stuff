b33a5f0badd03addeea3040030e837df0a52af00
Armao131 <armando_imeraj@outlook.com>
2025-08-31 22:36:28 +0300

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -11,6 +11,7 @@ from sklearn.base import clone
 skfolds = StratifiedKFold(n_splits=3)  # add shuffle=True if the dataset is
                                        # not already shuffled
 
+#The following code does roughly the same thing as Scikit-Learn’s cross_val_score() function, and it prints the same result
 for train_index, test_index in skfolds.split(X_train, y_train_5): #Assuming X_train, Y_train are np.array
     clone_clf = clone(sgd_clf)
     X_train_folds = X_train[train_index]
@@ -23,16 +24,80 @@ for train_index, test_index in skfolds.split(X_train, y_train_5): #Assuming X_tr
     n_correct = sum(y_pred == y_test_fold)
     print(n_correct / len(y_pred))  # prints 0.95035, 0.96035, and 0.9604
 
+#Confusion Matrix
+from sklearn.model_selection import cross_val_predict
 
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_train_5, y_train_pred)
+cm # np.array confusion matrix
+#=======================================================================
 
+#Evaluation Metrics
+#===================
 
+# Presicion = TP/(TP+FP) TP is the number of true positives, and FP is the number of false positives.
+# Recall - TP/(TP+FN) 
+from sklearn.metrics import precision_score, recall_score
+precision_score(y_train_5, y_train_pred)  
+#0.8370879772350012
+recall_score(y_train_5, y_train_pred)
+#0.6511713705958311
 
+# F1 scrore = Αρμονικος μεσος ορος του presicion και recall
+from sklearn.metrics import f1_score
+f1_score(y_train_5, y_train_pred)
 
+#Precision-Recall curve
+from sklearn.metrics import precision_recall_curve
 
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,method="decision_function")
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
 
+plt.figure(figsize=(8, 4))  # extra code – it's not needed, just formatting
+plt.plot(thresholds, precisions[:-1], "b--", label="Precision", linewidth=2)
+plt.plot(thresholds, recalls[:-1], "g-", label="Recall", linewidth=2)
+plt.vlines(threshold, 0, 1.0, "k", "dotted", label="threshold")
 
+# extra code – this section just beautifies and saves Figure 3–5
+idx = (thresholds >= threshold).argmax()  # first index ≥ threshold
+plt.plot(thresholds[idx], precisions[idx], "bo")
+plt.plot(thresholds[idx], recalls[idx], "go")
+plt.axis([-50000, 50000, 0, 1])
+plt.grid()
+plt.xlabel("Threshold")
+plt.legend(loc="center right")
+# save_fig("precision_recall_vs_threshold_plot")
 
+plt.show()
+
+#ROC curve
+from sklearn.metrics import roc_curve
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+#Plot
+idx_for_threshold_at_90 = (thresholds <= threshold_for_90_precision).argmax()
+tpr_90, fpr_90 = tpr[idx_for_threshold_at_90], fpr[idx_for_threshold_at_90]
+
+plt.plot(fpr, tpr, linewidth=2, label="ROC curve")
+plt.plot([0, 1], [0, 1], 'k:', label="Random classifier's ROC curve")
+plt.plot([fpr_90], [tpr_90], "ko", label="Threshold for 90% precision")
+plt.show()
+#=======================================================================
+
+#Error Analysis 
+#===============
+from sklearn.metrics import ConfusionMatrixDisplay
+#Confusion Matrix
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+ConfusionMatrixDisplay.from_predictions(y_train, y_train_pred)
+plt.show()
+#Normalized Confusion Matrix
+ConfusionMatrixDisplay.from_predictions(y_train, y_train_pred,
+                                        normalize="true", values_format=".0%")
+plt.show()
 
 
 
